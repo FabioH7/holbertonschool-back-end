c9aa6cfe1d700b5d8b4f329f8befe125067f14d5
FabioH7 <fhysollari4@gmail.com>
2023-04-05 14:26:22 +0200

    adding comments

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Getting my first apis """
 import requests
 import json
 from sys import argv
@@ -8,23 +9,17 @@ todos_api = requests.get(
     'https://jsonplaceholder.typicode.com/todos/')
 user_api = requests.get(
     'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
-
 todo_data = todos_api.text
 user_data = user_api.text
-
 user = json.loads(user_data)
 todos = json.loads(todo_data)
-
-
 completed = []
 all_todos = 0
-
 for todo in todos:
     if todo['userId'] == user['id']:
         if todo['completed']:
             completed.append(todo)
         all_todos += 1
-
 print(
     'Employee {} is done with tasks({}/{}):'.format(user['name'], len(completed), all_todos))
 for finished_todo in completed:
