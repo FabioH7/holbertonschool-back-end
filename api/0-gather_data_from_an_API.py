d84f6b055ed717e2d6aa2b1d6b7383f56eec4059
FabioH7 <fhysollari4@gmail.com>
2023-04-05 14:30:49 +0200

    adding comments

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,24 +3,26 @@
 import requests
 import json
 from sys import argv
-""" Getting my first apis """
 
-todos_api = requests.get(
-    'https://jsonplaceholder.typicode.com/todos/')
-user_api = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
-todo_data = todos_api.text
-user_data = user_api.text
-user = json.loads(user_data)
-todos = json.loads(todo_data)
-completed = []
-all_todos = 0
-for todo in todos:
-    if todo['userId'] == user['id']:
-        if todo['completed']:
-            completed.append(todo)
-        all_todos += 1
-print(
-    'Employee {} is done with tasks({}/{}):'.format(user['name'], len(completed), all_todos))
-for finished_todo in completed:
-    print('\t {}'.format(finished_todo['title']))
+
+if __name__ == "__main__":
+    """Get api bob"""
+    todos_api = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/')
+    user_api = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    todo_data = todos_api.text
+    user_data = user_api.text
+    user = json.loads(user_data)
+    todos = json.loads(todo_data)
+    completed = []
+    all_todos = 0
+    for todo in todos:
+        if todo['userId'] == user['id']:
+            if todo['completed']:
+                completed.append(todo)
+            all_todos += 1
+    print(
+        'Employee {} is done with tasks({}/{}):'.format(user['name'], len(completed), all_todos))
+    for finished_todo in completed:
+        print('\t {}'.format(finished_todo['title']))
